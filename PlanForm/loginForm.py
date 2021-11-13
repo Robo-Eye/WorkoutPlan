626b562e2bdfdab00e3ea1fbbdec83f546b4fd80
John Burke <burkejr19@gcc.edu>
2021-11-13 14:38:01 -0500

    test commit

--- PlanForm/loginForm.py
+++ PlanForm/loginForm.py
@@ -6,6 +6,13 @@ from wtforms.validators import InputRequired
 class LoginForm(FlaskForm):
     username = StringField("Username: ", validators=[InputRequired()])
     password = PasswordField("Password: ", validators=[InputRequired()])
+
+    submit = SubmitField("Submit")
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username: ", validators=[InputRequired()])
+    password = PasswordField("Password: ", validators=[InputRequired()])
+    confirm_password = PasswordField("Confirm Password: ", validators=[InputRequired()])
     email = EmailField("Email: ", validators=[InputRequired()])
 
     submit = SubmitField("Submit")
