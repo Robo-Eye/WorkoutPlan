17d7fd7f28da587bf59354097b5b474bc76851df
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-11-14 15:45:08 -0500

    Added hmtl to newPlan.j2 & added imports to WOPlan

--- PlanForm/WOPlan.py
+++ PlanForm/WOPlan.py
@@ -1,8 +1,14 @@
 import flask
+import os
 from flask.app import Flask
 from flask import render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
+scriptdir = os.path.abspath(os.path.dirname(__file__))
+dbpath = os.path.join(scriptdir, 'workoutPlan.sqlite3')
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'RandomPhraseToKeepItSafe'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbpath}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
