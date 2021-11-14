d7b7e5e699f3671dd5ab513715ec27d9d8b90ea3
John Burke <burkejr19@gcc.edu>
2021-11-14 17:26:27 -0500

    login and resgister mostly complete

--- PlanForm/loginForm.py
+++ PlanForm/loginForm.py
@@ -1,18 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, EqualTo, Length, Email
 
 class LoginForm(FlaskForm):
-    username = StringField("Username: ", validators=[InputRequired()])
+    email = StringField("Email: ", validators=[InputRequired()])
     password = PasswordField("Password: ", validators=[InputRequired()])
 
-    submit = SubmitField("Submit")
+    submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username: ", validators=[InputRequired()])
-    password = PasswordField("Password: ", validators=[InputRequired()])
-    confirm_password = PasswordField("Confirm Password: ", validators=[InputRequired()])
-    email = EmailField("Email: ", validators=[InputRequired()])
+    email = EmailField("Email: ", validators=[InputRequired(), Email()])
+    password = PasswordField("Password: ", validators=[InputRequired(), Length(min=8, max=256)])
+    confirm_password = PasswordField("Confirm Password: ", validators=[InputRequired(), EqualTo("password")])
 
-    submit = SubmitField("Submit")
+    submit = SubmitField("Register")
---
+++ PlanForm/scripts/registerCheck.js
@@ -0,0 +1,40 @@
+// listener for when document loads
+window.addEventListener("DOMContentLoaded", function() {
+    // get password fields
+    let pass = document.getElementById("pass");
+    let confPass = document.getElementById("confPass");
+
+    // if change in either field, check to see if the password are the same and if it meets length requirements
+    pass.addEventListener("change", checkPassword);
+    confPass.addEventListener("change", checkPassword)
+});
+
+function checkPassword(){
+    // get fields
+    let pass = document.getElementById("pass");
+    let confPass = document.getElementById("confPass");
+
+    // if not long enough
+    if (pass.value.length < 8){
+        pass.setCustomValidity("Password needs to be 8 or more characters");
+    }
+    // if too long
+    else if (pass.value.length > 256){
+        pass.setCustomValidity("Password is too long");
+    }
+
+    // if they are equal
+    if (pass.value === confPass.value){
+        pass.setCustomValidity("");
+        confPass.setCustomValidity("");
+    }
+    // if they are not equal
+    else{
+        // tell user they don't match
+        pass.setCustomValidity("");
+        confPass.setCustomValidity("Passwords do not match");
+    }
+
+
+}
+
