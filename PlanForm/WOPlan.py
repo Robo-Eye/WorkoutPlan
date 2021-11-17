39127ebbc6be03100dda36540db9661f8f12b7c5
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-11-17 15:00:42 -0500

    entered workouts to their tables

--- PlanForm/WOPlan.py
+++ PlanForm/WOPlan.py
@@ -28,41 +28,136 @@ class UserForm(db.Model):
     goals = db.Column(db.Unicode, nullable=False)
     frequency = db.Column(db.Integer, nullable=False)
 
+
 class Abs(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Chest(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Shoulders(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Back(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Biceps(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Triceps(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
+
 
 class Legs(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     workouts = db.Column(db.Unicode)
-    link_to_wo= db.Column(db.Unicode)
+    link_to_wo = db.Column(db.Unicode)
 
 
 db.drop_all()
 db.create_all()
 
-multiple_instances = [
-  
+multiple_abs = [
+    Abs(workouts="Ab crunches",
+        link_to_wo="https://www.youtube.com/watch?v=Xyd_fa5zoEU"),
+    Abs(workouts="Russian twists",
+        link_to_wo="https://www.youtube.com/watch?v=JyUqwkVpsi8"),
+    Abs(workouts="Plank hold",
+        link_to_wo="https://www.youtube.com/watch?v=Oi9pmH45W7A"),
+    Abs(workouts="Leg raises",
+        link_to_wo="https://www.youtube.com/watch?v=ttdkm6ESUjI"),
+    Abs(workouts="Knee raises",
+        link_to_wo="https://www.youtube.com/watch?v=KNzJ3GuIpB8")
 ]
+db.session.add_all(multiple_abs)
+db.session.commit()
+multiple_chest = [
+    Chest(workouts="Flat barbell bench press",
+          link_to_wo="https://www.youtube.com/watch?v=-MAABwVKxok"),
+    Chest(workouts="Dumbbell chest fly",
+          link_to_wo="https://youtu.be/eozdVDA78K0"),
+    Chest(workouts="Dumbbell pullover",
+          link_to_wo="https://youtu.be/tpLnfSQJ0gg"),
+    Chest(workouts="Flat machine bench press",
+          link_to_wo="https://youtu.be/xUm0BiZCWlQ"),
+    Chest(workouts="Cable crossover",
+          link_to_wo="https://www.youtube.com/watch?v=taI4XduLpTk")
+]
+db.session.add_all(multiple_chest)
+db.session.commit()
+multiple_shoulders = [
+    Shoulders(workouts="Seated barbell shoulder press",
+              link_to_wo="https://www.youtube.com/watch?t=4s&v=oBGeXxnigsQ"),
+    Shoulders(workouts="Seated dumbbell shoulder press",
+              link_to_wo="https://youtu.be/qEwKCR5JCog"),
+    Shoulders(workouts="Arnold press",
+              link_to_wo="https://youtu.be/6Z15_WdXmVw"),
+    Shoulders(workouts="Dumbbell lateral raises",
+              link_to_wo="https://youtu.be/3VcKaXpzqRo"),
+    Shoulders(workouts="Machine shoulder press",
+              link_to_wo="https://www.youtube.com/watch?t=2s&v=Wqq43dKW1TU")
+]
+db.session.add_all(multiple_shoulders)
+db.session.commit()
+
+multiple_back = [
+    Back(workouts="", link_to_wo=""),
+    Back(workouts="", link_to_wo=""),
+    Back(workouts="", link_to_wo=""),
+    Back(workouts="", link_to_wo=""),
+    Back(workouts="", link_to_wo="")
+]
+db.session.add_all(multiple_back)
+db.session.commit()
+
+multiple_biceps = [
+    Biceps(workouts="", link_to_wo=""),
+    Biceps(workouts="", link_to_wo=""),
+    Biceps(workouts="", link_to_wo=""),
+    Biceps(workouts="", link_to_wo=""),
+    Biceps(workouts="", link_to_wo="")
+]
+db.session.add_all(multiple_biceps)
+db.session.commit()
+
+multiple_triceps = [
+    Triceps(workouts="", link_to_wo=""),
+    Triceps(workouts="", link_to_wo=""),
+    Triceps(workouts="", link_to_wo=""),
+    Triceps(workouts="", link_to_wo=""),
+    Triceps(workouts="", link_to_wo="")
+]
+db.session.add_all(multiple_triceps)
+db.session.commit()
+
+multiple_legs = [
+    Legs(workouts="", link_to_wo=""),
+    Legs(workouts="", link_to_wo=""),
+    Legs(workouts="", link_to_wo=""),
+    Legs(workouts="", link_to_wo=""),
+    Legs(workouts="", link_to_wo=""),
+    Legs(workouts="", link_to_wo="")
+]
+db.session.add_all(multiple_legs)
+db.session.commit()
+
 
 @app.get('/workoutform/')
 def get_blank_form():
