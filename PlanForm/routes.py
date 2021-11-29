a8c9f93b67af3fa166affa7493cf199c97fac397
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-11-29 15:44:08 -0500

    testing uncomment

--- PlanForm/routes.py
+++ PlanForm/routes.py
@@ -136,105 +136,105 @@ class Workouts(db.Model):
     #userform_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
 
 
-# db.drop_all()  # for testing
-# db.create_all()
+db.drop_all()  # for testing
+db.create_all()
 
-# multiple_abs = [
-#     Abs(workouts="Ab crunches",
-#         link_to_wo="https://www.youtube.com/watch?v=Xyd_fa5zoEU"),
-#     Abs(workouts="Russian twists",
-#         link_to_wo="https://www.youtube.com/watch?v=JyUqwkVpsi8"),
-#     Abs(workouts="Plank hold",
-#         link_to_wo="https://www.youtube.com/watch?v=Oi9pmH45W7A"),
-#     Abs(workouts="Leg raises",
-#         link_to_wo="https://www.youtube.com/watch?v=ttdkm6ESUjI"),
-#     Abs(workouts="Knee raises",
-#         link_to_wo="https://www.youtube.com/watch?v=KNzJ3GuIpB8")
-# ]
-# db.session.add_all(multiple_abs)
-# db.session.commit()
-# multiple_chest = [
-#     Chest(workouts="Flat barbell bench press",
-#           link_to_wo="https://www.youtube.com/watch?v=-MAABwVKxok"),
-#     Chest(workouts="Dumbbell chest fly",
-#           link_to_wo="https://youtu.be/eozdVDA78K0"),
-#     Chest(workouts="Dumbbell pullover",
-#           link_to_wo="https://youtu.be/tpLnfSQJ0gg"),
-#     Chest(workouts="Flat machine bench press",
-#           link_to_wo="https://youtu.be/xUm0BiZCWlQ"),
-#     Chest(workouts="Cable crossover",
-#           link_to_wo="https://www.youtube.com/watch?v=taI4XduLpTk")
-# ]
-# db.session.add_all(multiple_chest)
-# db.session.commit()
-# multiple_shoulders = [
-#     Shoulders(workouts="Seated barbell shoulder press",
-#               link_to_wo="https://www.youtube.com/watch?t=4s&v=oBGeXxnigsQ"),
-#     Shoulders(workouts="Seated dumbbell shoulder press",
-#               link_to_wo="https://youtu.be/qEwKCR5JCog"),
-#     Shoulders(workouts="Arnold press",
-#               link_to_wo="https://youtu.be/6Z15_WdXmVw"),
-#     Shoulders(workouts="Dumbbell lateral raises",
-#               link_to_wo="https://youtu.be/3VcKaXpzqRo"),
-#     Shoulders(workouts="Machine shoulder press",
-#               link_to_wo="https://www.youtube.com/watch?t=2s&v=Wqq43dKW1TU")
-# ]
-# db.session.add_all(multiple_shoulders)
-# db.session.commit()
+multiple_abs = [
+    Abs(workouts="Ab crunches",
+        link_to_wo="https://www.youtube.com/watch?v=Xyd_fa5zoEU"),
+    Abs(workouts="Russian twists",
+        link_to_wo="https://www.youtube.com/watch?v=JyUqwkVpsi8"),
+    Abs(workouts="Plank hold",
+        link_to_wo="https://www.youtube.com/watch?v=Oi9pmH45W7A"),
+    Abs(workouts="Leg raises",
+        link_to_wo="https://www.youtube.com/watch?v=ttdkm6ESUjI"),
+    Abs(workouts="Knee raises",
+        link_to_wo="https://www.youtube.com/watch?v=KNzJ3GuIpB8")
+]
+db.session.add_all(multiple_abs)
+db.session.commit()
+multiple_chest = [
+    Chest(workouts="Flat barbell bench press",
+          link_to_wo="https://www.youtube.com/watch?v=-MAABwVKxok"),
+    Chest(workouts="Dumbbell chest fly",
+          link_to_wo="https://youtu.be/eozdVDA78K0"),
+    Chest(workouts="Dumbbell pullover",
+          link_to_wo="https://youtu.be/tpLnfSQJ0gg"),
+    Chest(workouts="Flat machine bench press",
+          link_to_wo="https://youtu.be/xUm0BiZCWlQ"),
+    Chest(workouts="Cable crossover",
+          link_to_wo="https://www.youtube.com/watch?v=taI4XduLpTk")
+]
+db.session.add_all(multiple_chest)
+db.session.commit()
+multiple_shoulders = [
+    Shoulders(workouts="Seated barbell shoulder press",
+              link_to_wo="https://www.youtube.com/watch?t=4s&v=oBGeXxnigsQ"),
+    Shoulders(workouts="Seated dumbbell shoulder press",
+              link_to_wo="https://youtu.be/qEwKCR5JCog"),
+    Shoulders(workouts="Arnold press",
+              link_to_wo="https://youtu.be/6Z15_WdXmVw"),
+    Shoulders(workouts="Dumbbell lateral raises",
+              link_to_wo="https://youtu.be/3VcKaXpzqRo"),
+    Shoulders(workouts="Machine shoulder press",
+              link_to_wo="https://www.youtube.com/watch?t=2s&v=Wqq43dKW1TU")
+]
+db.session.add_all(multiple_shoulders)
+db.session.commit()
 
-# multiple_back = [
-#     Back(workouts="Sumo deadlift", link_to_wo="https://youtu.be/1v4r9hht_K4"),
-#     Back(workouts="Bent over rows", link_to_wo="https://youtu.be/9efgcAjQe7E"),
-#     Back(workouts="T-bar rows", link_to_wo="https://youtu.be/j3Igk5nyZE4"),
-#     Back(workouts="Single-arm rows", link_to_wo="https://youtu.be/pYcpY20QaE8"),
-#     Back(workouts="Lat pulldowns – wide grip",
-#          link_to_wo="https://youtu.be/CAwf7n6Luuc")
-# ]
-# db.session.add_all(multiple_back)
-# db.session.commit()
+multiple_back = [
+    Back(workouts="Sumo deadlift", link_to_wo="https://youtu.be/1v4r9hht_K4"),
+    Back(workouts="Bent over rows", link_to_wo="https://youtu.be/9efgcAjQe7E"),
+    Back(workouts="T-bar rows", link_to_wo="https://youtu.be/j3Igk5nyZE4"),
+    Back(workouts="Single-arm rows", link_to_wo="https://youtu.be/pYcpY20QaE8"),
+    Back(workouts="Lat pulldowns – wide grip",
+         link_to_wo="https://youtu.be/CAwf7n6Luuc")
+]
+db.session.add_all(multiple_back)
+db.session.commit()
 
-# multiple_biceps = [
-#     Biceps(workouts="E-Z bar bicep curl",
-#            link_to_wo="https://www.youtube.com/watch?v=zG2xJ0Q5QtI"),
-#     Biceps(workouts="Barbell preacher curl",
-#            link_to_wo="https://www.youtube.com/watch?v=nbcgEmZ0Be4"),
-#     Biceps(workouts="Dumbbell bicep curl",
-#            link_to_wo="https://youtu.be/sAq_ocpRh_I"),
-#     Biceps(workouts="Hammer curls", link_to_wo="https://youtu.be/zC3nLlEvin4"),
-#     Biceps(workouts="Incline bench dumbbell curl",
-#            link_to_wo="https://youtu.be/soxrZlIl35U")
-# ]
-# db.session.add_all(multiple_biceps)
-# db.session.commit()
+multiple_biceps = [
+    Biceps(workouts="E-Z bar bicep curl",
+           link_to_wo="https://www.youtube.com/watch?v=zG2xJ0Q5QtI"),
+    Biceps(workouts="Barbell preacher curl",
+           link_to_wo="https://www.youtube.com/watch?v=nbcgEmZ0Be4"),
+    Biceps(workouts="Dumbbell bicep curl",
+           link_to_wo="https://youtu.be/sAq_ocpRh_I"),
+    Biceps(workouts="Hammer curls", link_to_wo="https://youtu.be/zC3nLlEvin4"),
+    Biceps(workouts="Incline bench dumbbell curl",
+           link_to_wo="https://youtu.be/soxrZlIl35U")
+]
+db.session.add_all(multiple_biceps)
+db.session.commit()
 
-# multiple_triceps = [
-#     Triceps(workouts="Close-grip bench press",
-#             link_to_wo="https://youtu.be/cXbSJHtjrQQ"),
-#     Triceps(workouts="Skull crushers",
-#             link_to_wo="https://youtu.be/QXzhjRnYRT0"),
-#     Triceps(workouts="Seated overhead extension",
-#             link_to_wo="https://youtu.be/YbX7Wd8jQ-Q"),
-#     Triceps(workouts="Tricep kickbacks",
-#             link_to_wo="https://www.youtube.com/watch?v=bxPoVw8_khE"),
-#     Triceps(workouts="Close grip pushups",
-#             link_to_wo="https://www.youtube.com/watch?v=yy_stTZs5-4)")
-# ]
-# db.session.add_all(multiple_triceps)
-# db.session.commit()
+multiple_triceps = [
+    Triceps(workouts="Close-grip bench press",
+            link_to_wo="https://youtu.be/cXbSJHtjrQQ"),
+    Triceps(workouts="Skull crushers",
+            link_to_wo="https://youtu.be/QXzhjRnYRT0"),
+    Triceps(workouts="Seated overhead extension",
+            link_to_wo="https://youtu.be/YbX7Wd8jQ-Q"),
+    Triceps(workouts="Tricep kickbacks",
+            link_to_wo="https://www.youtube.com/watch?v=bxPoVw8_khE"),
+    Triceps(workouts="Close grip pushups",
+            link_to_wo="https://www.youtube.com/watch?v=yy_stTZs5-4)")
+]
+db.session.add_all(multiple_triceps)
+db.session.commit()
 
-# multiple_legs = [
-#     Legs(workouts="Barbell back squats",
-#          link_to_wo="https://youtu.be/1oed-UmAxFs"),
-#     Legs(workouts="Dumbbell Bulgarian split squat",
-#          link_to_wo="https://youtu.be/2C-uNgKwPLE"),
-#     Legs(workouts="Standing calf raises",
-#          link_to_wo="https://www.youtube.com/watch?v=wxwY7GXxL4k"),
-#     Legs(workouts="Leg press", link_to_wo="https://youtu.be/IZxyjW7MPJQ"),
-#     Legs(workouts="Leg extension",
-#          link_to_wo="https://www.youtube.com/watch?t=21s&v=YyvSfVjQeL0")
-# ]
-# db.session.add_all(multiple_legs)
-# db.session.commit()
+multiple_legs = [
+    Legs(workouts="Barbell back squats",
+         link_to_wo="https://youtu.be/1oed-UmAxFs"),
+    Legs(workouts="Dumbbell Bulgarian split squat",
+         link_to_wo="https://youtu.be/2C-uNgKwPLE"),
+    Legs(workouts="Standing calf raises",
+         link_to_wo="https://www.youtube.com/watch?v=wxwY7GXxL4k"),
+    Legs(workouts="Leg press", link_to_wo="https://youtu.be/IZxyjW7MPJQ"),
+    Legs(workouts="Leg extension",
+         link_to_wo="https://www.youtube.com/watch?t=21s&v=YyvSfVjQeL0")
+]
+db.session.add_all(multiple_legs)
+db.session.commit()
 
 
 @app.get('/workoutform/')
