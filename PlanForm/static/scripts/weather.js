a60d1dd17bf45a07b9a3c408f426f317cc7901ff
John Burke <burkejr19@gcc.edu>
2021-12-07 13:11:12 -0500

    added bootstrap for weather

--- PlanForm/static/scripts/weather.js
+++ PlanForm/static/scripts/weather.js
@@ -28,27 +28,76 @@ function getWeather(data){
 function addWeather(data){
     const weatherDiv = document.getElementById("weather");
 
-    const period = data.properties.periods[0];
+    const container = document.createElement("div");
+    container.classList.add("container-fluid");
+    weatherDiv.appendChild(container);
 
-    // add time
-    const time = document.createElement("p");
-    time.innerText = `Time: ${period.startTime} - ${period.endTime}`;
-    weatherDiv.appendChild(time);
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
 
-    // add weather image
-    const img = document.createElement("img");
-    img["src"] = period.icon;
-    weatherDiv.appendChild(img);
+    const periods = data.properties.periods;
 
-    // add temperature
-    const temp = document.createElement("p");
-    temp.innerText = `Temperature: ${period.temperature}${period.temperatureUnit}`;
-    weatherDiv.appendChild(temp);
+    const numHoursShown = 3;
+    for (let p = 0; p < numHoursShown; p++){
+        period = periods[p];
+        //make div for col
+        const col = document.createElement("div");
+        col.classList.add("col-md-4");
+        col.classList.add("justify-content-center");
+        col.classList.add("text-center");
+        col.classList.add("border");
+        col.classList.add("border-top-0");
+        col.classList.add("border-bottom-0");
+        col.classList.add("border-dark");
+        row.appendChild(col);
 
-    // add forcast
-    const forecast = document.createElement("p");
-    forecast.innerText = `Forecast: ${period.shortForecast}`;
-    weatherDiv.appendChild(forecast);
+        // add time
+        let start = period.startTime;
+        let end = period.endTime;
+        let pos = start.indexOf("T");
+        start = start.charAt(pos+1) + start.charAt(pos+2);
+        pos = end.indexOf("T");
+        end = end.charAt(pos+1) + end.charAt(pos+2);
+
+        if (start == 12)
+            start += "p.m."
+        else if (start == 0)
+            start = 12 + "a.m."
+        else if (start > 12)
+            start = (start-12) + "p.m."
+        else
+            start += "a.m."
+
+        if (end == 12)
+            end += "p.m."
+        else if (end == 0)
+            end = 12 + "a.m."
+        else if (end > 12)
+            end = (end-12) + "p.m."
+        else
+            end += "a.m."
+        
+        const time = document.createElement("p");
+        time.innerText = `${start} - ${end}`;
+        col.appendChild(time);
+
+        // add weather image
+        const img = document.createElement("img");
+        img["src"] = period.icon;
+        img["alt"] = "weather icon"
+        col.appendChild(img);
+
+        // add temperature
+        const temp = document.createElement("p");
+        temp.innerText = `${period.temperature}\u00B0 ${period.temperatureUnit}`;
+        col.appendChild(temp);
+
+        // add forcast
+        const forecast = document.createElement("p");
+        forecast.innerText = `${period.shortForecast}`;
+        col.appendChild(forecast);
+    }
 
 }
 
